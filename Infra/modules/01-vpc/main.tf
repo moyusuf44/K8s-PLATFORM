4fd8fcb32b904b2f5c7c3c9e91cb05366a4e0b46
moyusuf44 <mohamedyusuf.6445@gmail.com>
2026-09-17 15:40:53 +0300

    vpc fix

--- Infra/modules/01-vpc/main.tf
+++ Infra/modules/01-vpc/main.tf
@@ -1,20 +1,20 @@
 module "vpc" {
-    source                  = "terraform-aws-modules/vpc/aws"
-    version                 = "5.8.1"
+    source  = "terraform-aws-modules/vpc/aws"
+    version = "5.8.1"
 
-    name                    = "moyusufs-k8s-app"
-    cidr                    = "10.0.0.0/22"
+    name = "moyusufs-k8s-app"
+    cidr = "10.0.0.0/21"
 
-    azs                     = ["eu-north-1a", "eu-north-1b", "eu-north-1c"] 
-    public_subnets          = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+    azs            = ["eu-north-1a", "eu-north-1b", "eu-north-1c"] 
+    public_subnets = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
 
-    enable_nat_gateway      = false
-    single_nat_gateway      = false 
+    enable_nat_gateway = false
+    single_nat_gateway = false 
 
-    enable_dns_support      = true
-    enable_dns_hostnames    = true
+    enable_dns_support   = true
+    enable_dns_hostnames = true
 
-    map_ip_on_launch        = true
+    map_public_ip_on_launch = true
 
     tags = {
         Terraform  = true
@@ -22,7 +22,7 @@ module "vpc" {
     }
 }
 
-resource "ecs_security_group" "this" {
+resource "aws_security_group" "this" {
     name   = "k8s-app-sg"
     vpc_id = module.vpc.vpc_id
 
