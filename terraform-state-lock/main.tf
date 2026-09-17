b6de1734140c8acc159b654cb1c33d9be34fa108
moyusuf44 <mohamedyusuf.6445@gmail.com>
2026-09-17 14:51:14 +0300

    terraform state lock

---
+++ terraform-state-lock/main.tf
@@ -0,0 +1,12 @@
+resource "dynamodb_table" "terraform-k8sapp-state-lock" {
+    name         = "terraform-k8sapp-state-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+
